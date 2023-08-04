102e0a3e32e10e906a6826d88ef0362f932b9d12
kno777 <kno0109@mail.ru>
2023-08-04 17:43:20 +0400

    improved commenController when user add comment

--- InstagramCloneFirebase/InstagramCloneFirebase/Controllers/Comment/CommentController.swift
+++ InstagramCloneFirebase/InstagramCloneFirebase/Controllers/Comment/CommentController.swift
@@ -8,47 +8,16 @@
 import UIKit
 import Firebase
 
-class CommentController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+class CommentController: UICollectionViewController, UICollectionViewDelegateFlowLayout, CustomInputAccessoryViewDelegate {
     
-    var post: UserPostModel?
-    var comments: [CommentModel] = []
-    
-    lazy var containerView: UIView = {
-        let containerView = UIView()
-        containerView.backgroundColor = .white
-        containerView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 50)
-        
-        let submitButton = UIButton(type: .system)
-        submitButton.setTitle("Submit", for: .normal)
-        submitButton.translatesAutoresizingMaskIntoConstraints = false
-        submitButton.setTitleColor(.systemBlue, for: .normal)
-        submitButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
-        submitButton.addTarget(self, action: #selector(handleSubmit), for: .touchUpInside)
-        
-        containerView.addSubview(submitButton)
-        submitButton.anchor(top: containerView.topAnchor, left: nil, bottom: containerView.bottomAnchor, right: containerView.trailingAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: -12, width: 50, height: 0)
-        
-        containerView.addSubview(self.commentTextField)
-        self.commentTextField.anchor(top: containerView.topAnchor, left: containerView.leadingAnchor, bottom: containerView.bottomAnchor, right: submitButton.leadingAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
-        
-        return containerView
-    }()
-    
-    let commentTextField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "Enter Comment"
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        return textField
-    }()
-    
-    @objc private func handleSubmit() {
-        print("Inserting comment: ", self.commentTextField.text ?? "")
+    func didSubmit(for comment: String) {
+        print("Inserting comment: ", comment)
         
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
         guard let postId = self.post?.id else { return }
         
-        let values = ["text": commentTextField.text ?? "", "creationDate": Date().timeIntervalSince1970, "uid": uid] as [String: Any]
+        let values = ["text": comment, "creationDate": Date().timeIntervalSince1970, "uid": uid] as [String: Any]
         
         Database.database().reference().child("comments").child(postId).childByAutoId().updateChildValues(values) { err, ref in
             
@@ -58,9 +27,21 @@ class CommentController: UICollectionViewController, UICollectionViewDelegateFlo
             }
             
             print("Successfully added user comments")
+            
+            self.containerView.clearCommentWhenSuccessued()
         }
     }
-
+    
+    
+    var post: UserPostModel?
+    var comments: [CommentModel] = []
+    
+    lazy var containerView: CustomInputAccessoryView = {
+        let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 50)
+        let customInputAccessoryView = CustomInputAccessoryView(frame: frame)
+        customInputAccessoryView.delegate = self
+        return customInputAccessoryView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- InstagramCloneFirebase/InstagramCloneFirebase/Helpers/CustomInputAccessoryView.swift
+++ InstagramCloneFirebase/InstagramCloneFirebase/Helpers/CustomInputAccessoryView.swift
@@ -7,45 +7,75 @@
 
 import UIKit
 
-class CustomInputAccessoryView: UIView {
+protocol CustomInputAccessoryViewDelegate {
+    func didSubmit(for comment: String)
+}
+
+class CustomInputAccessoryView: UIView, UITextViewDelegate {
+    
+    var delegate: CustomInputAccessoryViewDelegate?
+    
+    let commentTextView: UITextView = {
+        let textView = UITextView()
+        textView.isScrollEnabled = false
+        textView.text = "Enter comment..."
+        textView.font = .systemFont(ofSize: 18)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    
+    private lazy var submitedButton: UIButton = {
+        let submitButton = UIButton(type: .system)
+        submitButton.setTitle("Submit", for: .normal)
+        submitButton.translatesAutoresizingMaskIntoConstraints = false
+        submitButton.setTitleColor(.systemBlue, for: .normal)
+        submitButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        submitButton.addTarget(self, action: #selector(handleSubmit), for: .touchUpInside)
+        return submitButton
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        autoresizingMask = .flexibleHeight
         
-        let containerView = UIView()
-        containerView.backgroundColor = .white
-        containerView.translatesAutoresizingMaskIntoConstraints = false
-        //containerView.frame = CGRect(x: 0, y: 0, width: frame.width, height: 50)
+        commentTextView.delegate = self
         
-        addSubview(containerView)
+        addSubview(submitedButton)
+        addSubview(commentTextView)
         
-        containerView.anchor(top: nil, left: safeAreaLayoutGuide.leadingAnchor, bottom: bottomAnchor, right: safeAreaLayoutGuide.trailingAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
+        self.submitedButton.anchor(top: topAnchor, left: nil, bottom: safeAreaLayoutGuide.bottomAnchor, right: trailingAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: -12, width: 50, height: 0)
         
-        let submitButton = UIButton(type: .system)
-        submitButton.translatesAutoresizingMaskIntoConstraints = false
-        submitButton.setTitle("Submit", for: .normal)
-        submitButton.setTitleColor(.systemBlue, for: .normal)
-        submitButton.addTarget(self, action: #selector(handelSubmit), for: .touchUpInside)
-        submitButton.titleLabel?.font = .boldSystemFont(ofSize: 14)
-        
-        containerView.addSubview(submitButton)
-        
-        submitButton.anchor(top: containerView.topAnchor, left: nil, bottom: containerView.bottomAnchor, right: containerView.trailingAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: -12, width: 50, height: 0)
-        
-        let textField = UITextField()
-        textField.placeholder = "Enter a comment"
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        
-        containerView.addSubview(textField)
-        
-        textField.anchor(top: nil, left: containerView.leadingAnchor, bottom: containerView.bottomAnchor, right: submitButton.leadingAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
+        self.commentTextView.anchor(top: topAnchor, left: leadingAnchor, bottom: safeAreaLayoutGuide.bottomAnchor, right: submitedButton.leadingAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return .zero
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    @objc private func handleSubmit() {
+        guard let comment = commentTextView.text, comment.count > 0 else { return}
+        delegate?.didSubmit(for: comment)
+    }
     
-    @objc private func handelSubmit() {
-        
+    func clearCommentWhenSuccessued() {
+        self.commentTextView.text = nil
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = nil
+        textView.textColor = .lightGray
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "Enter comment..."
+            textView.textColor = .lightGray
+        }
     }
 }

b9063348ed306c9f5fe60ca5ced1d770976302d2
kno777 <kno0109@mail.ru>
2023-08-08 17:18:17 +0400

    created AppsSearchController and also added a few custom logic for add constraints and refactor code

--- AppStoreJSONApi/AppStoreJSONApi/Controller/AppsSearchController.swift
+++ AppStoreJSONApi/AppStoreJSONApi/Controller/AppsSearchController.swift
@@ -7,10 +7,25 @@
 
 import UIKit
 
-final class AppsSearchController: UICollectionViewController {
+final class AppsSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: "cellId")
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 350)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! SearchResultCell
+        return cell
     }
 }
---
+++ AppStoreJSONApi/AppStoreJSONApi/Views/SearchResultCell.swift
@@ -0,0 +1,90 @@
+//
+//  SearchResultCell.swift
+//  AppStoreJSONApi
+//
+//  Created by Kno Harutyunyan on 08.08.23.
+//
+
+import UIKit
+
+final class SearchResultCell: UICollectionViewCell {
+    
+    private lazy var appIconImageView: UIImageView = {
+       let image = UIImageView()
+        image.backgroundColor = .red
+        image.widthAnchor.constraint(equalToConstant: 64).isActive = true
+        image.heightAnchor.constraint(equalToConstant: 64).isActive = true
+        image.layer.cornerRadius = 12
+        //image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private lazy var appLabel: UILabel = {
+       let label = UILabel()
+        label.text = "APP NAME"
+        //label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var categoryLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Photos & Videos"
+        return label
+    }()
+    
+    private lazy var ratingsLabel: UILabel = {
+       let label = UILabel()
+        label.text = "9.26M"
+        return label
+    }()
+    
+    private lazy var getButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("GET", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        button.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 32).isActive = true
+        button.layer.cornerRadius = 16
+        return button
+    }()
+    
+    private lazy var screenshot1ImageView = self.createImageView()
+    private lazy var screenshot2ImageView = self.createImageView()
+    private lazy var screenshot3ImageView = self.createImageView()
+
+    private func createImageView() -> UIImageView {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .blue
+        return imageView
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+                
+        let labelStackView = VerticalStackView(arrangedSubviews: [appLabel, categoryLabel, ratingsLabel])
+                
+        let infoTopStackView = UIStackView(arrangedSubviews: [appIconImageView, labelStackView, getButton])
+                
+        infoTopStackView.spacing = 12
+        infoTopStackView.alignment = .center
+        
+        let appImagesStackView = UIStackView(arrangedSubviews: [screenshot1ImageView, screenshot2ImageView, screenshot3ImageView])
+        
+        appImagesStackView.spacing = 12
+        appImagesStackView.distribution = .fillEqually
+                
+        let overallStackView = VerticalStackView(arrangedSubviews: [infoTopStackView, appImagesStackView], spacing: 16)
+        overallStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(overallStackView)
+        
+        overallStackView.anchor(top: self.topAnchor, leading: self.leadingAnchor, bottom: self.bottomAnchor, trailing: self.trailingAnchor, padding: UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16))
+       
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
